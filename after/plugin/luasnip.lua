cf2934dc3e1aa8f34110e447f152ee73f10150bb
lavet13 <lavet13@mail.ru>
2026-04-06 14:16:46 +0300

    fix(luasnip): fixing path for lazy load

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,8 +1,6 @@
 local luasnip = require("luasnip");
 
-require("luasnip.loaders.from_vscode").lazy_load({
-  paths = vim.fn.stdpath("data") .. "/site/pack/packer/start/friendly-snippets"
-});
+require("luasnip.loaders.from_vscode").lazy_load();
 
 luasnip.config.setup({
   history = true,
