0b6c2a96d60164667b471e6641d62f90a2760788
lavet13 <lavet13@mail.ru>
2026-07-22 17:31:23 +0300

    fixed %, the ftplugins were disabled

--- lua/lavet13/set.lua
+++ lua/lavet13/set.lua
@@ -38,6 +38,3 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 vim.o.winborder = "rounded"
-
-vim.keymap.set({ "n", "x", "o" }, "%", "<Plug>(MatchitNormalForward)", { silent = true })
-vim.keymap.set({ "n", "x", "o" }, "g%", "<Plug>(MatchitNormalBackward)", { silent = true })
